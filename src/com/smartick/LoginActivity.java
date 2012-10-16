08cd157ebed05b88df37d9311cd3575f6b1d5ab0
Juan López <juan.lopez@smartick.es>
2012-10-16 14:55:18 +0000

    Almacenar usuarios logados
    git-svn-id: http://svn.smartick.es/svn/sva-smartick/trunk/smartickAndroid@6131 79f48f48-82ef-0310-9b43-d5d80899f613

--- src/com/smartick/LoginActivity.java
+++ src/com/smartick/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.smartick;
 
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -11,8 +14,8 @@ import java.net.URLEncoder;
 import android.R.drawable;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +31,7 @@ public class LoginActivity extends Activity {
 	
 	private static final String URL_CONTEXT = "http://www.smartick.es/";
 	private static final String URL_SMARTICK_LOGIN = URL_CONTEXT+"smartick_login";
+	private static final String USERS_FILE = "usersSmk";
 	
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -77,6 +81,11 @@ public class LoginActivity extends Activity {
     
     private void redirectLogin(){
         if(!urlResult.contains("acceso")){
+        	try {
+				negotiateStoreUsers();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
         	Intent intent = new Intent(this, MainActivity.class);
         	intent.putExtra("url", urlResult);
         	startActivity(intent);
@@ -129,6 +138,35 @@ public class LoginActivity extends Activity {
         String encodedLoginUserName = URLEncoder.encode(username.getText().toString(), "UTF-8");
         String encodedLoginPassword = URLEncoder.encode(password.getText().toString(), "UTF-8");
         return "j_username="+encodedLoginUserName+"&j_password="+encodedLoginPassword;
+    }
+    
+    private void negotiateStoreUsers() throws IOException{
+		String token = username.getText().toString()+"***"+password.getText().toString();
+    	try {
+			String fileContent = readUsersInStore();
+			if(!fileContent.toString().contains(token)){
+				saveUserInStore(token);
+			}
+		} catch (FileNotFoundException e) {
+			saveUserInStore(token);
+		}
+    }
+    
+    private void saveUserInStore(String token) throws IOException{
+		FileOutputStream usersFile = openFileOutput(USERS_FILE, Context.MODE_PRIVATE);
+		usersFile.write(token.getBytes());
+		usersFile.close();
+    }
+    
+    private String readUsersInStore() throws IOException{
+    	FileInputStream usersFile = openFileInput(USERS_FILE);
+		StringBuffer fileContent = new StringBuffer("");
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((length = usersFile.read(buffer)) != -1) {
+		    fileContent.append(new String(buffer));
+		}
+		return fileContent.toString();
     }
     
 }
