b1e107de6e16274453c8ee40230d65cef3f6bb5a
jjoorrggee <jjoorrggee@hotmail.com>
2015-02-10 14:16:22 +0100

    Ficheros antiguos

--- src/com/smartick/activities/TutorLoginActivity.java
+++
@@ -1,9 +0,0 @@
-package com.smartick.activities;
-
-import android.annotation.SuppressLint;
-import android.app.ListActivity;
-
-@SuppressLint("NewApi")
-public class TutorLoginActivity extends ListActivity {
-	//TO DO - login para tutores -> recuperar datos de tutores de db
-}
