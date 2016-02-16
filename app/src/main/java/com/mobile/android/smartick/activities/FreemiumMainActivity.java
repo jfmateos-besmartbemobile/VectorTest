dbf62b73b9dc18ee1d9a052f8923e2cb757000cf
Sergio <sergio.barrio.slocker@gmail.com>
2016-02-16 17:02:11 +0100

    AN-13	Login con Facebook

--- app/src/main/java/com/mobile/android/smartick/activities/FreemiumMainActivity.java
+++ app/src/main/java/com/mobile/android/smartick/activities/FreemiumMainActivity.java
@@ -234,7 +234,7 @@ public class FreemiumMainActivity extends Activity {
                         }else{
                             //session finished, we let the user go back
                             String urlWebView = webView.getUrl();
-                            if (urlWebView.contains("fin")){
+                            if (urlWebView != null && urlWebView.contains("fin")){
                                 doLogout();
                             }else{
                                 webView.evaluateJavascript("volverButtonPressedAndroidApp();",null);
