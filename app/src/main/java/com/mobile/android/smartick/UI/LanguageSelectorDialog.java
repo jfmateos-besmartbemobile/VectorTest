434a5f4814302f3d48cb35ff5f71ea527bb7d1b2
Sergio <sergio.barrio.slocker@gmail.com>
2016-05-24 13:49:54 +0200

    AN-12	Añadir botón de selección de idioma

--- app/src/main/java/com/mobile/android/smartick/UI/LanguageSelectorDialog.java
+++ app/src/main/java/com/mobile/android/smartick/UI/LanguageSelectorDialog.java
@@ -60,6 +60,98 @@ public class LanguageSelectorDialog extends DialogFragment{
             }
         });
 
+        Button es_AR_button = (Button) rootView.findViewById(R.id.lang_es_AR_button);
+        es_AR_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-AR");
+            }
+        });
+
+        Button es_BO_button = (Button) rootView.findViewById(R.id.lang_es_BO_button);
+        es_BO_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-BO");
+            }
+        });
+
+        Button es_CL_button = (Button) rootView.findViewById(R.id.lang_es_CL_button);
+        es_CL_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-CL");
+            }
+        });
+
+        Button es_CO_button = (Button) rootView.findViewById(R.id.lang_es_CO_button);
+        es_CO_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-CO");
+            }
+        });
+
+        Button es_EC_button = (Button) rootView.findViewById(R.id.lang_es_EC_button);
+        es_EC_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-EC");
+            }
+        });
+
+        Button es_MX_button = (Button) rootView.findViewById(R.id.lang_es_MX_button);
+        es_MX_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-MX");
+            }
+        });
+
+        Button es_PE_button = (Button) rootView.findViewById(R.id.lang_es_PE_button);
+        es_PE_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-PE");
+            }
+        });
+
+
+        Button es_PY_button = (Button) rootView.findViewById(R.id.lang_es_PY_button);
+        es_PY_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-PY");
+            }
+        });
+
+
+        Button es_UY_button = (Button) rootView.findViewById(R.id.lang_es_UY_button);
+        es_UY_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-UY");
+            }
+        });
+
+        Button es_VE_button = (Button) rootView.findViewById(R.id.lang_es_VE_button);
+        es_VE_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setLanguage("es-VE");
+            }
+        });
+
         return rootView;
     }
     private void setLanguage(String language){
