e4cd3ed144e289172e7f080a3d058263407f5719
Daniel Sanchez Ceinos <dsanchezc@vectoritcgroup.com>
2017-08-14 14:22:19 +0200

    Match seek bar with iOS

--- app/src/main/java/com/mobile/android/smartick/UI/CustomSeekBar.java
+++ app/src/main/java/com/mobile/android/smartick/UI/CustomSeekBar.java
@@ -1,17 +1,13 @@
 package com.mobile.android.smartick.UI;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.SeekBar;
-
-import com.mobile.android.smartick.util.Constants;
 
 /**
  * Created by sbarrio on 25/06/15.
@@ -19,97 +15,74 @@ import com.mobile.android.smartick.util.Constants;
 
 public class CustomSeekBar extends SeekBar {
 
-    Drawable mThumb;
+  private static final int textMargin = 6;
+  private static final int leftPlusRightTextMargins = textMargin + textMargin;
+  private static final int maxFontSize = 160;
+  private static final int minFontSize = 100;
 
-    private static final int textMargin = 6;
-    private static final int leftPlusRightTextMargins = textMargin + textMargin;
-    private static final int maxFontSize = 55;
-    private static final int minFontSize = 30;
+  private Rect textBounds;
 
-    protected String overlayText;
-    protected Paint textPaint;
+  protected String overlayText;
+  protected Paint textPaint;
 
-    public CustomSeekBar (Context context, AttributeSet attrs) {
-            super(context, attrs);
-            Resources resources = getResources();
 
-            //Set up drawn text attributes here
-            textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            textPaint.setTypeface(Typeface.DEFAULT_BOLD);
-            textPaint.setTextAlign(Paint.Align.LEFT);
-            textPaint.setColor(Color.WHITE);
+  public CustomSeekBar(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    //Set up drawn text attributes here
+    textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    textPaint.setTextAlign(Paint.Align.LEFT);
+    Typeface tfBoogaloo = Typeface.createFromAsset(getContext().getAssets(), "fonts/Boogaloo-Regular.otf");
+    textPaint.setTypeface(tfBoogaloo);
+    textPaint.setColor(Color.WHITE);
+    textPaint.setStrokeWidth(10);
+    textPaint.setTextSize(maxFontSize);
+    textBounds = new Rect();
+  }
+
+  //This attempts to ensure that the text fits inside your SeekBar on a resize
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    setFontSmallEnoughToFit(w - leftPlusRightTextMargins);
+  }
+
+  //Finds the largest text size that will fit
+  protected void setFontSmallEnoughToFit(int width) {
+    int textSize = maxFontSize;
+    textPaint.setTextSize(textSize);
+    while ((textPaint.measureText("  ") > width) && (textSize > minFontSize)) {
+      textSize--;
+      textPaint.setTextSize(textSize);
     }
+  }
 
-    //This attempts to ensure that the text fits inside your SeekBar on a resize
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        setFontSmallEnoughToFit(w - leftPlusRightTextMargins);
-    }
+  //Clients use this to change the displayed text
+  public void setOverlayText(String text) {
+    this.overlayText = text;
+    invalidate();
+  }
 
-    //Finds the largest text size that will fit
-    protected void setFontSmallEnoughToFit(int width) {
-        int textSize = maxFontSize;
-        textPaint.setTextSize(textSize);
-        while ((textPaint.measureText("  ") > width) && (textSize > minFontSize)) {
-            textSize--;
-            textPaint.setTextSize(textSize);
-        }
-    }
+  //Draws the text onto the SeekBar
+  @Override
+  protected synchronized void onDraw(Canvas canvas) {
+    //Draw everything else (i.e., the usual SeekBar) first
+    super.onDraw(canvas);
 
-    //Clients use this to change the displayed text
-    public void setOverlayText(String text) {
-        this.overlayText = text;
-        invalidate();
-    }
+    //No text, no problem
+    if (overlayText == null || overlayText.length() == 0) return;
 
-    //Draws the text onto the SeekBar
-    @Override
-    protected synchronized void onDraw(Canvas canvas) {
-        //Draw everything else (i.e., the usual SeekBar) first
-        super.onDraw(canvas);
+    canvas.save();
 
-        //No text, no problem
-        if(overlayText!= null  ||overlayText.length() == 0) {
-            return;
-        }
+    textPaint.getTextBounds(overlayText, 0, overlayText.length(), textBounds);
 
-        canvas.save();
+    //Finally, just draw the text on top of the SeekBar
+    float x = getThumb().getBounds().centerX() - 4 * textBounds.width() / 6;
+    float y = getThumb().getBounds().centerY() + 3 * textBounds.height() / 6;
+    canvas.drawText(overlayText, x, y, textPaint);
 
-        //Here are a few parameters that could be useful in calculating where to put the text
-        int width = this.getThumb().getMinimumWidth() - leftPlusRightTextMargins;
-        int height = this.getHeight();
-
-        //A somewhat fat finger takes up about seven digits of space
-        // on each side of the thumb; YFMV
-        int fatFingerThumbHangover = (int) textPaint.measureText("1234567");
-
-        float textWidth = textPaint.measureText(overlayText);
-
-        int progress = this.getProgress();
-        int maxProgress = this.getMax();
-        double percentProgress = (double) progress / (double) maxProgress;
-        int textHeight = (int) (Math.abs(textPaint.ascent()) + textPaint.descent() + 1);
-
-        int thumbOffset = this.getThumbOffset();
-
-        //These are measured from the point textMargin in from the left of the SeekBarWithText view.
-        int middleOfThumbControl = (int) ((double) width * percentProgress);
-        int spaceToLeftOfFatFinger = middleOfThumbControl - fatFingerThumbHangover;
-        int spaceToRightOfFatFinger = (width - middleOfThumbControl) - fatFingerThumbHangover;
-
-        int spaceToLeftOfThumbControl = middleOfThumbControl - thumbOffset;
-        int spaceToRightOfThumbControl = (width - middleOfThumbControl) - thumbOffset;
-
-        int bottomPadding = this.getPaddingBottom();
-        int topPadding = this.getPaddingTop();
-
-        //Finally, just draw the text on top of the SeekBar
-        float x = this.getThumb().getBounds().centerX() - textWidth / 2;
-        float y = this.getY();
-        canvas.drawText(overlayText, x + width - 100 ,y, textPaint);
-
-        canvas.restore();
-    }
+    canvas.restore();
+  }
 }
 
